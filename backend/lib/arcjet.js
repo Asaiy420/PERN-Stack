386db5ee7e0811a79af80ad649ca166d4909f929
Asaiy420 <kcaryan05@gmail.com>
2025-04-28 16:22:56 +0545

    fixed rate limit

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -1,31 +1,28 @@
-import arcjet, {tokenBucket, shield, detectBot} from "@arcjet/node";
-import dotenv from "dotenv"
+import arcjet, { tokenBucket, shield, detectBot, slidingWindow } from "@arcjet/node";
 
-dotenv.config();
+import "dotenv/config";
 
-// initializing arcjet
-
+// init arcjet
 export const aj = arcjet({
-    key: process.env.ARCJET_KEY,
-    //BOT DETECTION
-    characteristics: ["ipsrc"],
-    rules:[
-        // shield protects our site from attacks like, SQL Injection, XSS, CSRF, and other such.
-        shield({mode:"LIVE"}),
-        detectBot({
-            mode: "LIVE",
-            //block all bots except search engines
-            allow:[
-                "CATEGORY:SEARCH_ENGINE"
-            ]
-        }),
-        //RATE LIMITING
-        tokenBucket({
-            mode:"LIVE",
-            refillRate: 5,
-            interval:10,
-            capacity:10,
-        })
-    ]
-
-})
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    // shield protects your app from common attacks e.g. SQL injection, XSS, CSRF attacks
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      // block all bots except search engines
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        // see the full list at https://arcjet.com/bot-list
+      ],
+    }),
+    // rate limiting
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 60,
+      capacity: 10,
+    }),
+  ],
+});
