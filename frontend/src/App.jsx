d25535dcb95efb47ac2e7aa6682dd5c8f2ea77cd
Asaiy420 <kcaryan05@gmail.com>
2025-04-28 11:56:51 +0545

    added dynamic theme selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 import ProductPage from "./pages/ProductPage";
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
+
+  const {theme} = useThemeStore(); // Zustand store for theme management
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme="night">
+    // Set the theme dynamically
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}> 
       <Navbar />
 
       <Routes>
