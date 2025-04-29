e85a09f03e84a6f7b49c4916ebb467698d59c3ea
Asaiy420 <kcaryan05@gmail.com>
2025-04-29 10:57:54 +0545

    added product page

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -9,6 +9,7 @@ export const useProductStore = create((set, get) => ({
   products: [],
   loading: false,
   error: null,
+  currentProduct: null,
 
   // form state
   formData: {
@@ -25,7 +26,7 @@ export const useProductStore = create((set, get) => ({
     set({ loading: true });
 
     try {
-      const { formData } = get(); 
+      const { formData } = get();
       await axios.post(`${BASE_URL}/api/products`, formData);
       await get().fetchProducts();
       get().resetForm();
@@ -64,6 +65,41 @@ export const useProductStore = create((set, get) => ({
       toast.success("Product deleted successfully!");
     } catch (error) {
       console.log("Error when deleting the product", error);
+      toast.error("Something went wrong...");
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  fetchProduct: async (id) => {
+    set({ loading: true });
+    try {
+      const response = await axios.get(`${BASE_URL}/api/products/${id}`);
+      set({
+        currentProduct: response.data.data,
+        formData: response.data.data, // Set formData to the fetched product data
+        error: null,
+      });
+    } catch (error) {
+      console.log("Error when fetching a product", error);
+      set({ error: "Something went wrong", currentProduct: null });
+    } finally {
+      set({ loading: false });
+    }
+  },
+  updateProduct: async (id) => {
+    set({ loading: true });
+
+    try {
+      const { formData } = get();
+      const response = await axios.put(
+        `${BASE_URL}/api/products/${id}`,
+        formData
+      );
+      set({ currentProduct: response.data.data });
+      toast.success("Product has been updated successfully!");
+    } catch (error) {
+      console.log("Error when updating the product", error);
       toast.error("Something went wrong...");
     } finally {
       set({ loading: false });
